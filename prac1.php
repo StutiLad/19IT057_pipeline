91354b0de8e0ac1cbad5a84703a118ee8ce514ba
StutiLad <72381827+StutiLad@users.noreply.github.com>
2022-02-18 23:30:54 +0530

    Add files via upload

---
+++ prac1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    class Person{
+        private $fname;
+        private $id;
+        // Constructor
+        public function __construct($fname, $id){
+            echo "Initialising the object...<br/>";
+            $this->fname = $fname;
+            $this->id = $id;
+        }
+        // Destructor
+        public function __destruct(){
+            echo "Destroying Object...";
+        }
+        // public method to show name
+        public function showName(){
+            echo "My name is " . $this->fname . ", ID is " . $this->id . "<br/>";
+        }
+    }
+    // creating class object
+    $john = new Person("Stuti Lad", "19IT057");
+    $john->showName();
+
+    ?>
+</body>
+</html>
